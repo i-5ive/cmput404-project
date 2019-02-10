94bb65477c2c59a2ec1dd03776f722ea91fde92f
hreherch <hreherch@ualberta.ca>
2019-02-10 16:20:39 -0700

    tests fixed

--- react/src/js/test/unit/react/AppTest.js
+++ react/src/js/test/unit/react/AppTest.js
@@ -6,6 +6,6 @@ import { shallow } from "enzyme";
 describe("App Tests", () => {
     it("Renders react-router without throwing errors.", () => {
         const app = shallow(<App />);
-        expect(app.getElement().type.name).to.equal("BrowserRouter");
+        expect(app.getElement().props.className).to.equal("core-app-view");
     });
 });
