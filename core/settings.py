027a14c4039f61fa31048e824f2ed364d04b4f5e
Chaitali Patel <chaitali_patel@intuit.com>
2019-02-10 23:56:10 -0700

    jk tests failing

--- core/settings.py
+++ core/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cmput',
-        'USER': 'chaitali',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
