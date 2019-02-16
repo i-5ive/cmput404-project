39bdcc52eb4218bc1168c2a920ae59ae030e655e
MandyMeindersma <meinders@ualberta.ca>
2019-02-16 12:01:43 -0700

    adding a comment

--- react/src/js/react/posts/NewPostButton.jsx
+++ react/src/js/react/posts/NewPostButton.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
+/**
+ * This is componenet will allow the user to make a new post
+ * This should only be shown if there is a user logged in
+ */
 export default class NewPostButton extends React.Component {
+    //TODO: only allow this to show if there is a user logged in
+    // <img src="../../icons/new-post.svg" alt="Make a new post">
     render() {
         return (
-            <button className="new-post-button">
+            <button className="new-post-button" type="button">
                 NewPostButton
             </button>
         );
