3e28d76509561bc854426f2b357ad0f1194403ae
NO <no>
2019-02-19 17:05:46 -0700

    reverted karma conf

--- react/karma.conf.js
+++ react/karma.conf.js
@@ -85,8 +85,7 @@ module.exports = function(config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        // TODO: change this back
-        browsers: ["Firefox"],//["Chrome"],
+        browsers: ["Chrome"],
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
