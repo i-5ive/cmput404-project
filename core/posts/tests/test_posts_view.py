657d6858b426be4bb4b5c16a2591d18e92b5e30a
Chaitali Patel <chaitali_patel@intuit.com>
2019-02-23 19:08:39 -0700

    fix build fail

--- core/posts/tests/test_posts_view.py
+++ core/posts/tests/test_posts_view.py
@@ -25,9 +25,8 @@ class PostViewsTest(TestCase):
             "categories": ["cool", "fun", "sad"]
         }
         post_data = json.dumps(data)
-        response = self.client.post('/posts/', {'query': 'createpost', 'postData': post_data})
+        response = self.client.post('/posts/', {'postData': post_data})
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data["query"], "createpost")
         self.assertEqual(response.data["message"], "Post created")
 
     # From https://stackoverflow.com/questions/11170425/how-to-unit-test-file-upload-in-django, credits to Danilo Cabello
@@ -39,7 +38,7 @@ class PostViewsTest(TestCase):
         }
         post_data = json.dumps(data)
         fp = SimpleUploadedFile("file.jpg", b"file_content", content_type="image/jpeg")
-        response = self.client.post('/posts/', {'imageFiles': fp, 'query': 'createpost', 'postData': post_data})
+        response = self.client.post('/posts/', {'imageFiles': fp, 'postData': post_data})
         self.assertEqual(response.status_code, 200)
 
     def test_invalid_file_type(self):
@@ -50,7 +49,7 @@ class PostViewsTest(TestCase):
         }
         post_data = json.dumps(data)
         fp = SimpleUploadedFile("file.mp4", b"file_content", content_type="video/mp4")
-        response = self.client.post('/posts/', {'imageFiles': fp, 'query': 'createpost', 'postData': post_data})
+        response = self.client.post('/posts/', {'imageFiles': fp, 'postData': post_data})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data["message"], "Invalid file type")
 
@@ -63,7 +62,7 @@ class PostViewsTest(TestCase):
         post_data = json.dumps(data)
         fp = SimpleUploadedFile("file.jpg", b"file_content", content_type="image/jpeg")
         fp2 = SimpleUploadedFile("file2.jpg", b"file_content", content_type="image/png")
-        response = self.client.post('/posts/', {'imageFiles': {fp, fp2}, 'query': 'createpost', 'postData': post_data})
+        response = self.client.post('/posts/', {'imageFiles': {fp, fp2}, 'postData': post_data})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(Posts.objects.filter(author=author_id)), 3)
     
@@ -74,7 +73,7 @@ class PostViewsTest(TestCase):
             "title": "wild"
         }
         post_data = json.dumps(data)
-        response = self.client.post('/posts/', {'query': 'createpost', 'postData': post_data})
+        response = self.client.post('/posts/', {'postData': post_data})
 
         post = Posts.objects.get(author=author_id)
         response = self.client.delete(f'/posts/{post.id}/')
