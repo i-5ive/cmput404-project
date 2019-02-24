95c1b0f9e2cf6aa400d66bab962e778f574c7c8f
Chaitali Patel <cpatel1@ualberta.ca>
2019-02-23 21:00:22 -0700

    Handle create posts with images files  (#53)
    * finish posts backend
    
    * ADD: delete post backend functionality
    
    * ADD: delete post backend functionality
    
    * Initial changes to post feed + comments can be made (not connected to backend) (#47)
    
    * initial changes to front end, I have the posts feed somewhat working and there is supposed to be a new post button, nothing is connected to the back end
    
    * adding a comment
    
    * adding some unit tests but I am having errors with istanbull and I can not run them
    
    * changing variables
    
    * fixing the icons loading problem
    
    * final changes to front end for day 1, the comments should work, once I get to chaitalis back end
    
    * lol oops, deleting this file
    
    * fixing the tests
    
    * changing packe-lock and cscc
    
    * fixing some linting errors
    
    * fixing the pr requests
    
    * lol fixing build break
    
    * View and respond to friend requests (#54)
    
    * friend request view updates
    
    * finished unit tests
    
    * reverted karma conf
    
    * fixed tests
    
    * fixed overflow for notifications
    
    * View, edit profiles (#55)
    
    * almost finished profile editing and viewing
    
    * finished profile updating, reading, added tests to backend
    
    * fixed eslint errors
    
    * added unit tests for form validation
    
    * added documentation for util.py
    
    * placeholder for befriending/unfriending in profile
    
    * made home button on header work and fixed warnings
    
    * display error and success messages, display loader
    
    * finished remaining endpoints for follow/unfollow and checking
    
    * ran eslint
    
    * improvements everywhere
    
    * removed storing display_name in friend request model
    
    * ran eslint fix
    
    * it works yeet
    
    * Added file size check and fixed request
    
    * fix build fail
    
    * PR comments
    
    * PR comments

---
+++ core/posts/posts_view.py
@@ -0,0 +1,74 @@
+import base64
+import json
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from core.posts.models import Posts
+from core.authors.models import Author
+from core.posts.serializers import PostsSerializer
+
+from core.authors.util import get_author_id
+
+MAX_UPLOAD_SIZE = 10485760
+    
+def create_post(request):
+    success = True
+    message = "Post created"
+
+    body = request.data
+    data = json.loads(body["postData"])
+
+    post_serializer = PostsSerializer(data=data)
+    post_serializer.is_valid()
+    new_post = post_serializer.save()
+
+    # if image, make another post with generated post_id
+    if (request.FILES):
+        # Credits to Justin Voss, https://stackoverflow.com/questions/851336/multiple-files-upload-using-same-input-name-in-django
+        for img_file in request.FILES.getlist('imageFiles'):
+            new_id = new_post.post_id
+            image_type = img_file.content_type
+            image_size = img_file.size
+            
+            if image_type == "image/jpeg" or image_type == "image/png":
+                content_type = f"{image_type};base64"
+            else:
+                return False, "Invalid file type", None
+
+            # http://www.learningaboutelectronics.com/Articles/How-to-restrict-the-size-of-file-uploads-with-Python-in-Django.php
+            if image_size > MAX_UPLOAD_SIZE:
+                return False, "The maximum file size that can be uploaded is 10MB", None
+
+            # Credits to Ykh, https://stackoverflow.com/questions/44489375/django-have-admin-take-image-file-but-store-it-as-a-base64-string
+            encoded_image = base64.encodestring(img_file.read())
+
+            data["post_id"] = new_id
+            data["content"] = str(encoded_image)
+            data["contentType"] = content_type
+
+            serializer = PostsSerializer(data=data)
+            serializer.is_valid()
+            serializer.save()
+
+    return success, message, post_serializer.data
+
+
+def handle_posts(request):
+    query = request.data["query"]
+    success = False
+    message = "Post not created"
+
+    success, message, new_post = create_post(request)
+
+    if success: 
+        code = 200
+    else:
+        code = 400
+
+    return Response({
+        "success": success,
+        "query": query,
+        "message": message,
+        "post": new_post
+    }, status=code)
--- core/posts/views.py
+++ core/posts/views.py
@@ -8,6 +8,7 @@ from rest_framework.response import Response
 
 from core.posts.models import Posts, Comments
 from core.posts.serializers import PostsSerializer, CommentsSerializer
+from core.posts.posts_view import handle_posts as handle_posts
 
 class PostsViewSet(viewsets.ModelViewSet):
     queryset = Posts.objects.all()
@@ -21,4 +22,20 @@ class PostsViewSet(viewsets.ModelViewSet):
         page= request.query_params.get('page', 1)
         serializer = CommentsSerializer(paginator.page(page), many=True, context={'request': request})
         return Response(serializer.data)
+    
+    def create(self, request):
+        return handle_posts(request)
+        
+    def destroy(self, request, pk=None):
+        # Use post_id to delete all related image posts too
+        post = Posts.objects.get(pk=pk)
+        Posts.objects.filter(post_id=post.post_id).delete()
+        return Response({
+            "success": True,
+            "message": "Post deleted successfully"
+        }, status=200)
+
+
+
+    
 
