418753b8f0ef841aef6d4d3bd85c95e5be41a003
Cole Mackenzie <colemackenzie1@gmail.com>
2019-03-07 16:14:30 -0700

    adding static root

--- core/settings.py
+++ core/settings.py
@@ -128,5 +128,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+#https://devcenter.heroku.com/articles/django-assets
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
