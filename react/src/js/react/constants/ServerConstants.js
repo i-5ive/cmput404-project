83aa0b38e4862a5ed14e4fe9b7a3bf488752293d
amalik2 <malik2@ualberta.ca>
2019-03-25 15:01:20 -0600

    udno server constants

--- react/src/js/react/constants/ServerConstants.js
+++ react/src/js/react/constants/ServerConstants.js
@@ -1,2 +1,1 @@
-// TODO: undo this
-export const SERVER_URL = process.env.NODE_ENV !== "development" ? "http://127.0.0.1:8000" : "https://cmput404-i5.herokuapp.com";
+export const SERVER_URL = process.env.NODE_ENV === "development" ? "http://127.0.0.1:8000" : "https://cmput404-i5.herokuapp.com";
