48e670d8ecdacb23d5d37d5baf8524b2209f2581
amalik2 <malik2@ualberta.ca>
2019-03-25 15:00:32 -0600

    fixed friend request issue

--- react/src/js/react/constants/ServerConstants.js
+++ react/src/js/react/constants/ServerConstants.js
@@ -1,1 +1,2 @@
-export const SERVER_URL = process.env.NODE_ENV === "development" ? "http://127.0.0.1:8000" : "https://cmput404-i5.herokuapp.com";
+// TODO: undo this
+export const SERVER_URL = process.env.NODE_ENV !== "development" ? "http://127.0.0.1:8000" : "https://cmput404-i5.herokuapp.com";
