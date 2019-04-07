25371f1e93a951c88c14b63be46d5d8219b3037e
Chaitali Patel <chaitali_patel@intuit.com>
2019-04-07 00:58:52 -0600

    oops

--- core/posts/views.py
+++ core/posts/views.py
@@ -174,7 +174,7 @@ class PostsViewSet(viewsets.ModelViewSet):
         data = post.content.split(",")[1]
         data = data.encode()
         data = base64.b64decode(data)
-        return HttpResponse(data, content_type="image/png")
+        return HttpResponse(data, content_type=post.contentType.split(";")[0])
 
     def update(self, request, pk):
         try:
